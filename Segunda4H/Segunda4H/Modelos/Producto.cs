464523182bb18ff32eb078a7481e01d11d3cad7a
damian <damian@LAB2-MAESTRO>
2024-04-22 12:24:02 -0600

    Agregar archivos de proyecto.

---
+++ Segunda4H/Segunda4H/Modelos/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Segunda4H.Modelos
+{
+    public class Producto
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "El inventario debe ser entero positivo")]
+        public int Inventario { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(200, ErrorMessage = "La longitud mázima son 200 caracteres")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar departamento")]
+        public string? Departamento { get; set; }
+    }
+}
