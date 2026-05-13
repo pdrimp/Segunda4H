6cb3edd1af50d7faac5c784aadfdf88bf7b89e0f
damian <damian@LAB2-MAESTRO>
2026-05-13 12:46:28 -0600

    con navegación

--- Segunda4H/Repositorios/IRepositorioPersonas.cs
+++ Segunda4H/Repositorios/IRepositorioPersonas.cs
@@ -4,6 +4,10 @@ namespace Segunda4H.Repositorios
 {
     public interface IRepositorioPersonas
     {
-        Task<List<Persona>> ObtenerPersonasAsync();
+        Task AgregarPersona(Persona persona);
+        Task<List<Persona>> ObtenerPersonas();
+        Task<Persona?> ObtenerPersonaPorId(int id);
+        Task ActualizarPersona(int id, Persona persona);
+        Task EliminarPersona(int id);
     }
 }
--- Segunda4H/Repositorios/RepositorioPersonas.cs
+++ Segunda4H/Repositorios/RepositorioPersonas.cs
@@ -12,9 +12,41 @@ namespace Segunda4H.Repositorios
             _context = context;
         }
 
-        public async Task<List<Persona>> ObtenerPersonasAsync()
+        public async Task ActualizarPersona(int id, Persona persona)
+        {
+            var personaExistente = _context.Personas.Find(id);
+            if (personaExistente == null)
+            {
+                throw new Exception("Persona no encontrada");
+            }
+            personaExistente.Nombre = persona.Nombre;
+            personaExistente.Correo = persona.Correo;
+            personaExistente.Telefono = persona.Telefono;
+            personaExistente.Genero = persona.Genero;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AgregarPersona(Persona persona)
+        {
+            await _context.Personas.AddAsync(persona);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EliminarPersona(int id)
+        {
+            await _context.Personas.Where(p => p.Id == id).ExecuteDeleteAsync();
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Persona?> ObtenerPersonaPorId(int id)
+        {
+            return await _context.Personas.FindAsync(id);
+        }
+
+        public async Task<List<Persona>> ObtenerPersonas()
         {
             return await _context.Personas.ToListAsync();
         }
+
     }
 }
